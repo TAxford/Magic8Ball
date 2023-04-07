837bff70735fd27317c12ae29bcafe24998ccb5e
Timothy Axford <t.axford2@yahoo.com>
2023-04-07 10:39:38 -0400

    Add project files.

---
+++ Magic8Ball/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Magic8Ball
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
